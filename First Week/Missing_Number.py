77ee27a34a6608ccc2bdad0843216bdf2bc5a450
ShubhamRao09 <raos19911@gmail.com>
2024-10-18 23:19:36 +0530

    Add files via upload

---
+++ First Week/Missing_Number.py
@@ -0,0 +1,17 @@
+# Array
+arr = [1, 2, 4, 5]
+
+# Largest element in array
+n = 5
+
+
+total_sum = n * (n + 1) // 2
+    
+# Sum of the array
+arr_sum = sum(arr)
+
+# Missing number
+missing_value = total_sum - arr_sum
+
+# Print missing number
+print("The missing number is:", missing_value)
