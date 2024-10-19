77c30a98e3daa4f0af128a8faebb597f4729e4c0
ShubhamRao09 <raos19911@gmail.com>
2024-10-20 00:03:57 +0530

    Add files via upload

---
+++ First Week/Second_Largest_Element.py
@@ -0,0 +1,18 @@
+def find_second_largest(array):
+    if len(array) < 2:
+        return "Array should have at least two elements"
+    
+    # Descending order
+    sorted_array = sorted(array, reverse=True)
+    
+    # Second largest element
+    return sorted_array[1]
+
+# Array
+numbers = [10, 24, 56, 75, 99]
+
+# Call function
+second_largest = find_second_largest(numbers)
+
+# Print
+print("The second largest element in the array is:", second_largest)
