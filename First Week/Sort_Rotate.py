d1f6e94ffe88b3d1e4a48e401b4ad6a1c0366d94
ShubhamRao09 <raos19911@gmail.com>
2024-10-19 18:12:20 +0530

    Add files via upload

---
+++ First Week/Sort_Rotate.py
@@ -0,0 +1,18 @@
+def check_sorted_and_rotated(arr):
+    n = len(arr)
+    
+    break_count = 0
+    
+    for i in range(n):
+        
+        if arr[i] > arr[(i + 1) % n]:
+            break_count += 1
+    
+    
+    return break_count == 1
+
+# Arrays
+arr1 = [3, 4, 5, 1, 2]
+
+# Print
+print(check_sorted_and_rotated(arr1))
